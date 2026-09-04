34ba0fd9d87d4458998cb43dd3edc448fb2b976c
hello-lamsey <lamsey@hello-robot.com>
2026-09-04 19:56:17 -0400

    wip nullspace velocity projection

--- demos/api_examples.py
+++ demos/api_examples.py
@@ -1,21 +1,31 @@
 import numpy as np
 import time
 
-from stretch4_body.robot.robot_client import RobotClient
+try:
+    from stretch4_body.robot.robot_client import RobotClient
+    from stretch4_kinematics.stretch_interface import StretchInterface
+except ImportError as e:
+    RobotClient = None
+    StretchInterface = None
 
-from stretch4_kinematics.stretch_interface import StretchInterface
 from stretch4_kinematics.kinematic_models import StretchKinematics
+from stretch4_kinematics.state import StretchJointPositions
 
 def main():
-    # Initialize the robot interface and kinematics solver
-    robot = RobotClient()
-    robot.startup()
-    interface = StretchInterface(robot=robot)
-    solver = StretchKinematics()
+    if RobotClient is not None and StretchInterface is not None:
+        # Initialize the robot interface and kinematics solver
+        robot = RobotClient()
+        robot.startup()
+        interface = StretchInterface(robot=robot)
 
-    # Query the current joint positions to use as the initial guess
-    # Zero out odom for correct base rotation
-    current_state = interface.get_joint_position(report_zero_odom=True)
+        # Query the current joint positions to use as the initial guess
+        # Zero out odom for correct base rotation
+        current_state = interface.get_joint_position(report_zero_odom=True)
+
+    else:
+        current_state = StretchJointPositions()
+
+    solver = StretchKinematics()
 
     # Solve 6-DOF Inverse Kinematics (base rotation allowed, no base translation)
     solved_pose = solver.inverse_6dof_local(
@@ -28,17 +38,17 @@ def main():
     print("Solved Joint Positions:")
     solved_pose.pretty_print()
 
-    # Command all joints to move to the solved pose simultaneously
-    ui = input("Enter y to move the robot to the solved pose.\nInput: ")
-    if ui == "y":
-        print("Moving!")
-        interface.move_to_local_pose(solved_pose)
-        time.sleep(3.)
-    else:
-        print("Move cancelled.")
-    
-    # Clean up connections
-    robot.stop()
+    if RobotClient is not None and StretchInterface is not None:
+        # Command all joints to move to the solved pose simultaneously
+        ui = input("Enter y to move the robot to the solved pose.\nInput: ")
+        if ui == "y":
+            print("Moving!")
+            interface.move_to_local_pose(solved_pose)
+            time.sleep(3.)
+        else:
+            print("Move cancelled.")
+
+        robot.stop()
 
 if __name__ == '__main__':
     main()
